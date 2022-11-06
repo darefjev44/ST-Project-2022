548528b4de535ec0b820a99f58058dc3d7f5e8b9
Keelan McCarthy <Keelan.McCarthy@students.ittralee.ie>
2022-11-06 15:16:52 +0000

    ==CHANGE LOG==
    Package Changes
    +NUnit
    
    Created Controllers
    +CustomerController
    +BankAccountController
    +TransactionController

---
+++ BankApp/Controllers/BankAccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankApp.Controllers
+{
+    public class BankAccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
