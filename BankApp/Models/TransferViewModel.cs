cd3c88729c573200ac1914d64649f45a36ad3168
darefjev44 <daniel.arefjev@students.ittralee.ie>
2022-11-14 19:28:25 +0000

    Deposit & Transfer View Models #44 #45 #50 #51

---
+++ BankApp/Models/TransferViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankApp.Models
+{
+    public class TransferViewModel
+    {
+        [Required]
+        [Range(0.01, 500.00,
+            ErrorMessage = "Amount must be between 0.01 and 500.00")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public int DestinationID { get; set; }
+    }
+}
