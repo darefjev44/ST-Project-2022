f43172cd77977604467d54176616476eb200b4c2
darefjev44 <daniel.arefjev@students.ittralee.ie>
2022-11-15 04:28:40 +0000

    Created a Base controller so that ~/ redirects to ~/Login, changed default route.

---
+++ BankApp/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankApp.Controllers
+{
+    public class BaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Index}/{id?}");
+    pattern: "{controller=Base}/{action=Index}/{id?}");
 
 app.Run();
