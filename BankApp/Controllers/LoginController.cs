fa3528f2d73279ec984330cb221b140869242545
darefjev44 <daniel.arefjev@students.ittralee.ie>
2022-11-21 08:33:40 +0000

    Signing in actually checks against DB - this *needs* to be done via proper authentication later.
    #115

--- BankApp/Controllers/LoginController.cs
+++ BankApp/Controllers/LoginController.cs
@@ -1,10 +1,19 @@
 ﻿using BankApp.Models;
+using BankApp.Models.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BankApp.Controllers
 {
     public class LoginController : Controller
     {
+
+        private readonly ApplicationDBContext _db;
+
+        public LoginController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,8 +25,10 @@ namespace BankApp.Controllers
         {
             if (ModelState.IsValid)
             {
+                var account = _db.Accounts.Find(lvm.UserID);
+
                 //if login details are correct, create session, redirect user, etc.
-                if (true)
+                if (account != null && account.PIN == lvm.PIN) //replace this with actual authentication for the love of god
                 {
                     return RedirectToAction("Index", "Home");
                 } 
