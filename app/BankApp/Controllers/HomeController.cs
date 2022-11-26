22d4344bbffc0986c9a2b69fa388dd7cda9cb15d
darefjev44 <daniel.arefjev@students.ittralee.ie>
2022-11-26 17:20:35 +0000

    Transfers implemented using Identity.
    AB#51 AB#96 AB#95

--- app/BankApp/Controllers/HomeController.cs
+++ app/BankApp/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Security.Cryptography.Xml;
 using System.Security.Principal;
 
 namespace BankApp.Controllers
@@ -85,51 +86,67 @@ namespace BankApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult Transfer(TransferViewModel transferViewModel)
+        public async Task<IActionResult> Transfer(TransferViewModel tvm)
         {
             if (ModelState.IsValid)
             {
-                /*
-                //check if destination id exists
-                var destUserFromDb = _dbContext.Accounts.Find(transferViewModel.DestinationID);
-
-                if (destUserFromDb != null)
+                //Check if destination account exists.
+                var destUser = await _userManager.FindByIdAsync(tvm.DestinationID.ToString());
+                if (destUser != null)
                 {
+                    //Get source account.
+                    var sourceUser = await _userManager.FindByIdAsync(User.Identity.Name);
+                    //Check if source account has enough balance.
+                    if(sourceUser.Balance >= tvm.Amount)
+                    {
+                        //Create transactions for both users.
+                        TransactionModel sourceTransaction = new TransactionModel(tvm.Amount, "TRANSFER TO " + tvm.DestinationID, DateTime.Today);
+                        TransactionModel destTransaction = new TransactionModel(tvm.Amount, "TRANSFER FROM " + User.Identity.Name, DateTime.Today);
 
-                    var sourceUserFromDb = _dbContext.Accounts.Find(1);
+                        //Update user balances.
+                        sourceUser.Balance -= tvm.Amount;
+                        destUser.Balance += tvm.Amount;
 
-                    //handle source user entity
-                    TransactionModel userTransaction = new TransactionModel();
-                    userTransaction.Amount = -transferViewModel.Amount;
-                    userTransaction.Date = DateTime.Today;
-                    userTransaction.Message = "TRANSFER - " + transferViewModel.DestinationID;
-                    sourceUserFromDb.Transactions.Add(userTransaction);
-                    sourceUserFromDb.Balance -= transferViewModel.Amount;
+                        //Add transactions to both users.
+                        sourceUser.Transactions.Add(sourceTransaction);
+                        destUser.Transactions.Add(destTransaction);
 
-                    //handle destination user entity
-                    TransactionModel destTransaction = new TransactionModel();
-                    destTransaction.Amount = transferViewModel.Amount;
-                    destTransaction.Date = DateTime.Today;
-                    destTransaction.Message = "TRANSFER - 1"; //sender's UserID here
-                    destUserFromDb.Transactions.Add(destTransaction);
-                    destUserFromDb.Balance += transferViewModel.Amount;
+                        //Ideally this would be done simultaniously so that if something goes wrong the transaction doesn't 'complete' on one side only.
+                        //Not 100% sure how to implement this - could possibly create backups of the users and put those up if something fails?
+                        var sourceResult = await _userManager.UpdateAsync(sourceUser);
+                        var destResult = await _userManager.UpdateAsync(destUser);
 
-                    _dbContext.Accounts.Update(sourceUserFromDb);
-                    _dbContext.Accounts.Update(destUserFromDb);
-                    _dbContext.SaveChanges();
-
-                    TempData["success"] = true;
-                    TempData["prompt-message"] = "Transferred €" + transferViewModel.Amount + " to User ID " + transferViewModel.DestinationID + " successfully.";
-
-                    ModelState.Clear(); //Clears the form, no need for it to be filled anymore.
+                        if(sourceResult.Succeeded && destResult.Succeeded)
+                        {
+                            TempData["prompt-title"] = "Transfer Successful!";
+                            TempData["prompt-body"] = @"
+                              <p>Successfully transfered €" + tvm.Amount + " to User ID " + tvm.DestinationID +
+                            ".<br>Your balance is now €" + sourceUser.Balance + ".</p>";
+                            ModelState.Clear();
+                        }
+                        else //userManager errors.
+                        {
+                            var errors = sourceResult.Errors.Concat(destResult.Errors);
+                            TempData["prompt-title"] = "Something went wrong.";
+                            TempData["prompt-body"] = "<p>Error!";
+                            foreach (var error in errors)
+                            {
+                                TempData["prompt-body"] += "<br>" + error;
+                            }
+                            TempData["prompt-body"] += "</p>";
+                        }
+                        return View();
+                    }
+                    //Not enough balance.
+                    TempData["prompt-title"] = "Transfer failed!";
+                    TempData["prompt-body"] = "<p>You do not have enough balance to complete this transaction.</p>";
                     return View();
-                } else
-                {
-                    //error handle please thanks
                 }
-                */
+                //Dest user doesn't exist.
+                TempData["prompt-title"] = "Transfer failed!";
+                TempData["prompt-body"] = "<p>User ID " + tvm.DestinationID + " could not be found.</p>";
             }
-            return View(transferViewModel);
+            return View();
         }
     }
 }
