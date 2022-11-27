a93561fe3be9ade5624c70d23c64a796bc821435
darefjev44 <daniel.arefjev@students.ittralee.ie>
2022-11-27 15:50:37 +0000

    Added helper class for model testing to count validation errors.
    AB#132

---
+++ app/BankApp/Tests/ModelTestHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankApp.Tests
+{
+    public class ModelTestHelper
+    {
+        public static IList<ValidationResult> Validate(object model)
+        {
+            var results = new List<ValidationResult>();
+            var validationContext = new ValidationContext(model, null, null);
+            Validator.TryValidateObject(model, validationContext, results, true);
+            if (model is IValidatableObject)
+            {
+                (model as IValidatableObject).Validate(validationContext);
+            } 
+
+            //Add validation errors to standard output
+            if(results.Count > 0)
+            {
+                foreach(var result in results)
+                {
+                    Console.WriteLine(result);
+                }
+            }
+
+            return results;
+        }
+    }
+}
