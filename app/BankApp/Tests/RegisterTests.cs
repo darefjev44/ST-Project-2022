83bb3717a26f25a08ebaa9b7755dd1996cc04214
darefjev44 <daniel.arefjev@students.ittralee.ie>
2022-11-27 17:04:01 +0000

    Phone number & Email unit tests for RegisterViewModel.
    DataType.EmailAddress is useless it seems.
    
    AB#23

--- app/BankApp/Tests/RegisterTests.cs
+++ app/BankApp/Tests/RegisterTests.cs
@@ -143,5 +143,117 @@ namespace BankApp.Tests
 
             Assert.AreEqual(1, result.Count);
         }
+
+        /* Phone Number */
+        //No input
+        [Test]
+        public void RegisterViewModel_NoPhoneNumber_Invalidates()
+        {
+            viewModel.PhoneNumber = "";
+
+            var result = ModelTestHelper.Validate(viewModel);
+
+            Assert.AreEqual(1, result.Count);
+        }
+
+        //Irish Number, no spaces.
+        [Test]
+        public void RegisterViewModel_PhoneNumberNoSpaces_Validates()
+        {
+            viewModel.PhoneNumber = "0861231234";
+
+            var result = ModelTestHelper.Validate(viewModel);
+
+            Assert.AreEqual(0, result.Count);
+        }
+
+        //Irish Number, no spaces.
+        [Test]
+        public void RegisterViewModel_PhoneNumberSpaces_Validates()
+        {
+            viewModel.PhoneNumber = "086 123 1234";
+
+            var result = ModelTestHelper.Validate(viewModel);
+
+            Assert.AreEqual(0, result.Count);
+        }
+
+        //Irish Number, International Format
+        [Test]
+        public void RegisterViewModel_PhoneNumberInternational_Validates()
+        {
+            viewModel.PhoneNumber = "+353 861231234";
+
+            var result = ModelTestHelper.Validate(viewModel);
+
+            Assert.AreEqual(0, result.Count);
+        }
+
+        //Invalid Number
+        [Test]
+        public void RegisterViewModel_InvalidPhoneNumber_Invalidates()
+        {
+            viewModel.PhoneNumber = "Not a phone number.";
+
+            var result = ModelTestHelper.Validate(viewModel);
+
+            Assert.AreEqual(1, result.Count);
+        }
+
+        /* Email */
+        //Empty Email
+        [Test]
+        public void RegisterViewModel_NoEmail_Invalidates()
+        {
+            viewModel.Email = "";
+
+            var result = ModelTestHelper.Validate(viewModel);
+
+            Assert.AreEqual(1, result.Count);
+        }
+
+        //Invalid Email
+        [Test]
+        public void RegisterViewModel_InvalidEmail_Invalidates()
+        {
+            viewModel.Email = "Not an email.";
+
+            var result = ModelTestHelper.Validate(viewModel);
+
+            Assert.AreEqual(1, result.Count);
+        }
+
+        //Invalid Email - without local-part
+        [Test]
+        public void RegisterViewModel_InvalidEmailMissingLocalPart_Invalidates()
+        {
+            viewModel.Email = "@email.com";
+
+            var result = ModelTestHelper.Validate(viewModel);
+
+            Assert.AreEqual(1, result.Count);
+        }
+
+        //Invalid Email - without @
+        [Test]
+        public void RegisterViewModel_InvalidEmailMissingAt_Invalidates()
+        {
+            viewModel.Email = "joebloggsemail.com";
+
+            var result = ModelTestHelper.Validate(viewModel);
+
+            Assert.AreEqual(1, result.Count);
+        }
+
+        //Invalid Email - without domain
+        [Test]
+        public void RegisterViewModel_InvalidEmailMissingDomain_Invalidates()
+        {
+            viewModel.Email = "joebloggs@";
+
+            var result = ModelTestHelper.Validate(viewModel);
+
+            Assert.AreEqual(1, result.Count);
+        }
     }
 }
