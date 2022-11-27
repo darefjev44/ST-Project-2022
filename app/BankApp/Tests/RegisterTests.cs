42c1bc59e0abf491f5173a2aa5f8a9f94b34ba94
darefjev44 <daniel.arefjev@students.ittralee.ie>
2022-11-27 15:58:39 +0000

    Added file to contain Register related tests.
    Currently contains a class to test RegisterViewModel tests, creating a viewModel and populating it with data that *should* be valid before each test, so the following tests can replace just the fields they need to test, rather than needing to additionally populate the rest of the fields with valid data.
    
    AB#23

---
+++ app/BankApp/Tests/RegisterTests.cs
@@ -0,0 +1,29 @@
+﻿using BankApp.Models;
+using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankApp.Tests
+{
+    [TestFixture]
+    public class RegisterViewModel_Validation
+    {
+        RegisterViewModel viewModel;
+
+        [SetUp]
+        public void SetUp()
+        {
+            viewModel = new RegisterViewModel();
+
+            /* Fill all params with values that should be valid. */
+
+            viewModel.FirstName = "Joe";
+            viewModel.LastName = "Bloggs";
+            viewModel.Address1 = "123 St.";
+            viewModel.City = "Citown";
+            viewModel.County = "Cork";
+            viewModel.Eircode = "A12 B34D";
+            viewModel.PhoneNumber = "086 123 1234";
+            viewModel.Email = "joe.bloggs@mail.ie";
+        }
+    }
+}
